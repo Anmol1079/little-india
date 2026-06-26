67681136b7e4813bb13411df984ae811f5e1246f
ANMOL-BAJRACHARYA <np03cs4s220440@heraldcollege.edu.np>
2026-06-26 14:28:29 +0545

    Main homepage Position Change

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,52 @@
-import Mainheader from "./components/headers/MainHeader";
-import SecondaryHeader from "./components/headers/SecondaryHeader";
-import Hero_1 from "./components/hero/Hero_1";
-import HeroVideo from "./components/hero/HeroVideo";
-import MainHeader from "./components/headers/MainHeader";
-import ThirdHeader from "./components/headers/ThirdHeader";
-import Hero_2 from "./components/hero/Hero_2";
-import FourthHeader from "./components/headers/FourthHeader";
+import Mainheader from "@/app/components/headers/MainHeader";
+import FourthHeader from "../../components/headers/FourthHeader";
+import NewDineno from "@/app/components/hero/NewDineno";
+import AboutUs from "@/app/components/about/AboutUs"; // Safe import pointing to your animated component
+import SpecialDishes from "@/app/components/specialdish/SpecialDishes";
+import Footer from "@/app/components/footer/Footer";
+import SpecialSection from "@/app/components/specialdish/SpecialSection";
+import VideoSection from "@/app/components/video/VideoChef";
+import MenuSection from "@/app/components/menu/MenuDish";
+import MenuDish from "@/app/components/menu/MenuDish";
+import OrderSection from "@/app/components/OrderSection/OrderSection";
+import ContactSection from "@/app/components/ContactForm/ContactSection";
+import LocationsSection from "@/app/components/find/LocationsSection";
+import SecondaryHeader from "@/app/components/headers/SecondaryHeader";
+import ThirdHeader from "@/app/components/headers/ThirdHeader";
+import FifthHeader from "@/app/components/headers/FifthHeader";
+import SixthHeader from "@/app/components/headers/SixthHeader";
+import TestimonialsSection from "@/app/components/testimonial/Testimonial";
+import HappyHourSection from "@/app/components/happyhour/HappyHour";
+import LunchBuffetSection from "@/app/components/lunchbuffet/LunchBuffet";
+import OurStorySection from "@/app/components/ourstory/Story";
+import WhyChooseUsSection from "@/app/components/whychooseus/WhyChooseUs";
+import BlogSection from "@/app/components/Blogs/Blogs";
+import FaqSection from "@/app/components/faq/Faq";
+import CtaSection from "@/app/components/ctasection/Cta";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-stone-950">
-      {/* <ThirdHeader /> */}
-
       <ThirdHeader />
-      <HeroVideo />
-      {/* <Hero_1 /> */}
+      <NewDineno />
+      {/* Renders the newly built, animated section */}
+      <AboutUs /> 
+      <SpecialDishes />
+      {/* The Zooming Video Component */}
+      <VideoSection />
+      <MenuDish />
+      <ContactSection />
+      <OrderSection />
+      <LocationsSection />
+      <TestimonialsSection  />
+      <HappyHourSection />
+      <LunchBuffetSection />
+      <OurStorySection />
+      <WhyChooseUsSection />
+      <FaqSection />
+      <BlogSection />
+      <CtaSection />
+      <Footer />
     </main>
   );
 }
